7cfbe5464d0acf49a3148759ac659cbe40d981e2
Jeremy Zou <392859231@qq.com>
2020-04-03 16:45:10 +0800

    up

--- mouse.py
+++ mouse.py
@@ -1,25 +1,73 @@
 import pyautogui
 import time
+import schedule
 
-L_pos = pyautogui.position()
+time.sleep(1)
 
+# L_pos = pyautogui.position()
+
+# try:
+#     while True:
+#         N_pos = pyautogui.position()
+#         if L_pos != N_pos:
+#             print(N_pos)
+#             L_pos = N_pos
+# except KeyboardInterrupt:
+#     print('\nExit')
+
+
+
+# new_pos = pyautogui.locateOnScreen('h.png')
+
+# goto_pos = pyautogui.center(new_pos)
+
+# pyautogui.moveTo(goto_pos)
+
+# pyautogui.click()
 try:
     while True:
-        N_pos = pyautogui.position()
-        if L_pos != N_pos:
-            print(N_pos)
-            L_pos = N_pos
+
+        pyautogui.scroll(-999999999)
+        pyautogui.scroll(-999999999)
+        pyautogui.scroll(-999999999)
+        pyautogui.scroll(-999999999)
+        
+        pyautogui.moveTo(x= 336, y= 782)
+
+        pyautogui.click()
+        
+        time.sleep(1)
+        
+        pyautogui.typewrite('py mouse.py')
+
+        pyautogui.press('enter')
+
+        pyautogui.moveTo(x= 195, y= 68)
+
+        pyautogui.click()
+
 except KeyboardInterrupt:
+
     print('\nExit')
 
 
-# def get_location():
-#     new_pos = pyautogui.locateOnScreen('h.png')
 
-#     goto_pos = pyautogui.center(new_pos)
 
-#     pyautogui.moveTo(goto_pos)
 
-#     pyautogui.click()
+# schedule.every().second.do(get_scroll)
+# schedule.every().second.do(get_location)
+# schedule.every().second.do(get_scroll2)
 
-# get_location()
+
+
+
+# try:
+#     while True:  
+#         schedule.run_pending()
+#         time.sleep(1)
+
+# except KeyboardInterrupt:
+#     print('\nExit')
+     
+
+            
--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import time
 import cv2 as cv
 import numpy as np
 import mss
+import schedule
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -12,69 +13,62 @@ time.sleep(2)
 #position = pyautogui.posith.png
 #print(position)
 
-old_pos = pyautogui.locateOnScreen('h.png')
+#old_pos = pyautogui.locateOnScreen('h.png')
 
 def get_location():
-    new_pos = pyautogui.locateOnScreen('g.png')
+    
+    pyautogui.scroll(-100000000)
 
-    goto_pos = pyautogui.center(new_pos)
+    time.sleep(0.05)
 
-    pyautogui.moveTo(goto_pos)
+    # new_pos = pyautogui.locateOnScreen('h.png')
+
+    # goto_pos = pyautogui.center(new_pos)
+
+    # pyautogui.moveTo(goto_pos)
+
+    # pyautogui.click()
+
+    pyautogui.moveTo(x= 336, y= 782)
 
     pyautogui.click()
 
-gu = cv.imread('g.png',0)
+    pyautogui.typewrite('py test.py')
+
+    pyautogui.press('enter')
+
+
+# schedule.every().second.do(get_location)
+
+gu = cv.imread('h.png', 0)
 #tw, th = gu.shape[::-1]
 
 with mss.mss() as sct:
     monitor = {'top': 0 , 'left': 0, 'width': 1280 , 'height': 800}
-    
+
 try:
-    while 'Screen capturing':
-        #pyautogui.scroll(-100000000)
-
-        #time.sleep(0.05)
-
+    while True:
+        
+        # threshold = 0.1
+        # loc2 = np.where(res2 >= threshold) 
+        # for pt in zip(*loc2[::-1]):
+        #     cv.rectangle(img, pt, (pt[0] + tw, pt[1] + th), (0, 0, 255), 2) 
+             
         img = np.array(sct.grab(monitor))
- 
+
         img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
         res2 = cv.matchTemplate(img_gray, gu, cv.TM_CCOEFF_NORMED)
 
-        # sift=cv.xfeatures2d.SIFT_create()
-        # kp1,des1=sift.detectAndCompute(res2,None)
-        # kp2,des2=sift.detectAndCompute(gu,None)
-
-        # #定义FLANN匹配器
-        # indexParams=dict(algorithm=0,trees=10)
-        # searchParams=dict(checks=50)
-        # flann=cv.FlannBasedMatcher(indexParams,searchParams)
-        # #使用KNN算法实现图像匹配，并对匹配结果排序
-        # matches=flann.knnMatch(des1,des2,k=2)
-        # matches=sorted(matches,key=lambda x:x[0].distance)
-            
-        #get_location()
-
-        # threshold = 0.1
- 
-        # loc2 = np.where(res2 >= threshold) 
-
-        # for pt in zip(*loc2[::-1]):
-        #     cv.rectangle(img, pt, (pt[0] + tw, pt[1] + th), (0, 0, 255), 2)      
-        
         get_location()
-
-    else:
-        pyautogui.scroll(-100000000)
-
-        time.sleep(0.05)
+        
+        # schedule.run_pending()
+        # time.sleep(1)
 
         #cv.imshow('OpenCV/Numpy normal', img)
-
-        # if cv.waitKey(0) == ord('q'):
-        #     cv.destroyAllWindows()
-        #     break
-
+        if cv.waitKey(0) == ord('q'):
+            cv.destroyAllWindows()
+            break
 
 except KeyboardInterrupt:
     print('\nExit')
@@ -95,4 +89,14 @@ except KeyboardInterrupt:
 
 #pyautogui.dragRel(300, None)
 
+# sift=cv.xfeatures2d.SIFT_create()
+        # kp1,des1=sift.detectAndCompute(res2,None)
+        # kp2,des2=sift.detectAndCompute(gu,None)
 
+        # #定义FLANN匹配器
+        # indexParams=dict(algorithm=0,trees=10)
+        # searchParams=dict(checks=50)
+        # flann=cv.FlannBasedMatcher(indexParams,searchParams)
+        # #使用KNN算法实现图像匹配，并对匹配结果排序
+        # matches=flann.knnMatch(des1,des2,k=2)
+        # matches=sorted(matches,key=lambda x:x[0].distance)
